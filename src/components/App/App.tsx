8e469034b3b58a6ec67955517c00b9d0bed5ab23
Ingo Köpp <ingokpp@gmail.com>
2021-05-25 20:35:37 +0200

    feat(client): remove unnecessary react boilerplate code. Add  component folder and move App into it

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const App = (): ReactElement => {
+	return (
+		<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+			Learn React
+		</a>
+	);
+};
+
+export default App;
